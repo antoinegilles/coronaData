16bf823284f83915c3c08935522f028c17fe353d
antoinegilles <antoinegillesapb@hotmail.com>
2022-02-27 12:37:54 +0100

    ajout de la consultation par région

--- src/App.js
+++ src/App.js
@@ -1,14 +1,94 @@
 import './App.css';
 import CoronaData from './coronaData';
+import CoronaDataRegion from './coronadataRegion';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import FlagIcon from '@mui/icons-material/Flag';
+import RoomIcon from '@mui/icons-material/Room';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-function App() {
+// Routeur
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  const [state, setState] = useState({
+    left: false,
+  });
+  const [dataDepartements, setDepartements] = useState([]);
+  const [listeDepartement, setListeDepartement] = useState(['']);
+
+
+
+  useEffect(() => {
+    axios.get('https://coronavirusapifr.herokuapp.com/data/live/departements').then((response) => {
+      setDepartements(response.data)
+
+      const data = []
+      let id = 0
+      response.data.forEach(element => {
+        id += 1
+        data.push({ 'id': id, 'departement': element.lib_dep })
+      });
+      setListeDepartement(data)
+
+    })
+
+
+
+  }, []);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        <ListItem button component={Link} to="/" button key={'france'}>
+          <ListItemIcon>
+            <FlagIcon />
+          </ListItemIcon>
+          <ListItemText primary={'France'} />
+        </ListItem>
+
+        <ListItem button component={Link} to="/region" key={'region'}>
+          <ListItemIcon>
+            <RoomIcon />
+          </ListItemIcon>
+          <ListItemText primary={'Par région'} />
+        </ListItem>
+      </List>
+    </Box>
+  );
+
   return (
-    <div className="App">
+    <div className='appBar'>
       <AppBar position="static" className='navBar'>
         <Toolbar>
           <IconButton
@@ -17,6 +97,7 @@ function App() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={toggleDrawer('left', true)}
           >
             <MenuIcon />
           </IconButton>
@@ -25,17 +106,23 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <div className='titleOne'>
-        <div>
-          <img className='imgLocation' src="./img/location.png" />
-        </div>
-        <div>
-          <h1 className='titre'>France</h1>
-        </div>
-      </div>
-      <CoronaData></CoronaData>
+      <Router>
+
+        {/* Menu */}
+        <Drawer
+          anchor={'left'}
+          open={state['left']}
+          onClose={toggleDrawer('left', false)}
+        >
+          {list('left')}
+        </Drawer>
+
+        <Routes>
+
+          <Route exact path="/" element={<CoronaData />} />
+          <Route exact path="/region" element={<CoronaDataRegion listeDepartement={listeDepartement} data={dataDepartements} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
-
-export default App;
--- src/coronadataRegion.js
+++ src/coronadataRegion.js
@@ -1,14 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-export default function CoronaData() {
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import { makeStyles } from '@mui/styles';
+
+
+export default function CoronaData(props) {
     const [data, set] = useState(0);
+    const [region, setRegion] = useState("");
+    const [departementChoisi, setDepartementChoisi] = useState('');
+
+
+    const useStyles = makeStyles({
+        select: {
+            '&:before': {
+                borderColor: 'white',
+            },
+            '&:after': {
+                borderColor: 'white',
+            },
+            'color': 'white',
+            'text-overflow': 'ellipsis',
+            'width':'10em'
+        },
+        icon: {
+            fill: 'white',
+        },
+    })
+
+    const classes = useStyles()
+
 
     useEffect(() => {
-        axios.get('https://coronavirusapifr.herokuapp.com/data/live/france').then((response) => {
-            set(response.data[0])
-        })
+        console.log(props)
     }, []);
+
+    const handleChange = (event) => {
+
+        setDepartementChoisi(event.target.value.departement);
+
+        axios.get('https://coronavirusapifr.herokuapp.com/data/live/departement/' + event.target.value.departement).then((response) => {
+            console.log(response)
+            set(response.data[0])
+
+        })
+
+    };
 
     const dateToday = () => {
         let today = new Date();
@@ -22,31 +64,66 @@ export default function CoronaData() {
 
     };
 
+
     return (
-        <div>
+        <div className='container'>
 
             <div className='titleOne'>
                 <div>
                     <img className='imgLocation' src="./img/location.png" />
                 </div>
                 <div>
-                    <h1 className='titre'>France</h1>
+                    <div className='contentRegionHeader'>
+                        <div>
+                            <h1 className='titre'>Département</h1>
+                        </div>
+                        <div className='selectRegion'>
+                            <Box sx={{ minWidth: 120 }}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-label" className='label'>Choisir</InputLabel>
+                                    <Select
+                                        labelId="demo-simple-select-label"
+                                        id="demo-simple-select"
+                                        defaultValue=""
+                                        className={classes.select}
+                                        value={departementChoisi.departement}
+                                        label="Département"
+                                        onChange={handleChange}
+                                        inputProps={{
+                                            classes: {
+                                                icon: classes.icon,
+                                                root: classes.root,
+                                            },
+                                        }}
+
+                                    >
+                                        {props.listeDepartement.map((departement, i) =>
+                                            <MenuItem key={i} value={departement}>{departement.departement}</MenuItem>
+                                        )
+                                        }
+
+                                    </Select>
+                                </FormControl>
+                            </Box>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className='dateDuJour'> Date : {dateToday()}</div>
+
 
             <div className='container'>
                 <div className='contenu'>
                     <div className='card'>
                         <div className='infos'>
                             <div className='logo'>
-                                <img src="./img/virus.png" />
+                                <img src="./img/dead.png" />
                             </div>
                             <div className='chiffre'>
-                                <b>{data.conf_j1}</b>
+                                <b>{data.incid_dchosp}</b>
                             </div>
                             <div className='libelle'>
-                                Nouveaux cas confirmés
+                                Décès à l’hôpital au cours des dernières 24h
                             </div>
                         </div>
                     </div>
